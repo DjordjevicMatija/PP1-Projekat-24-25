df93ca5ed81330ccf8918f1ca37978ae0c55e19e
Matija Djordjevic <djordjevic.matija2@gmail.com>
2025-02-02 23:27:45 +0100

    Implemented CUP parser program Const and Var declarations

--- workspace/MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
+++ workspace/MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -6,22 +6,23 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 
-import java_cup.runtime.Symbol;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
+import java_cup.runtime.Symbol;
+import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.util.Log4JUtils;
 
-public class MJTest {
+public class MJParserTest {
 
 	static {
 		DOMConfigurator.configure(Log4JUtils.instance().findLoggerConfigFile());
 		Log4JUtils.instance().prepareLogFile(Logger.getRootLogger());
 	}
 
-	public static void main(String[] args) throws IOException {
-		Logger log = Logger.getLogger(MJTest.class);
+	public static void main(String[] args) throws Exception {
+		Logger log = Logger.getLogger(MJParserTest.class);
+
 		Reader br = null;
 		try {
 
@@ -31,15 +32,22 @@ public class MJTest {
 			br = new BufferedReader(new FileReader(sourceCode));
 
 			Yylex lexer = new Yylex(br);
-			Symbol currToken = null;
-			while ((currToken = lexer.next_token()).sym != sym_old.EOF) {
-				if (currToken != null && currToken.value != null)
-					log.info(currToken.toString() + " " + currToken.value.toString());
-			}
+			MJParser p = new MJParser(lexer);
+
+			Symbol s = p.parse();
+			Program prog = (Program)(s.value);
+
+			log.info(prog.toString(""));
+			log.info("====================================");
+
+			RuleVisitor v = new RuleVisitor();
+			prog.traverseBottomUp(v);
+
+
+			// log.info("Print count calls = " + v.printCallCount);
 		}
 		finally {
 			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
 		}
 	}
-
 }
