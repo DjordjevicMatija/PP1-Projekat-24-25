90b17125b2720309ebf0c8b34bd4947c486a650d
Matija Djordjevic <djordjevic.matija2@gmail.com>
2025-09-19 00:02:40 +0200

    Added visit methods for Var and Method nodes

--- workspace/MJCompiler/src/rs/ac/bg/etf/pp1/SemanticPass.java
+++ workspace/MJCompiler/src/rs/ac/bg/etf/pp1/SemanticPass.java
@@ -3,13 +3,19 @@ package rs.ac.bg.etf.pp1;
 import org.apache.log4j.Logger;
 
 import rs.ac.bg.etf.pp1.ast.*;
-import rs.etf.pp1.symboltable.concepts.*;
+import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Struct;
 
 public class SemanticPass extends VisitorAdaptor {
     Logger log = Logger.getLogger(RuleVisitor.class);
 
     private boolean errorDetected = false;
+
     private Struct currentType = null;
+    private Obj currentMethod = null;
+
+    private boolean returnFound = false;
+    private boolean mainExists = false;
 
     // LOGS
     public void report_error(String message, SyntaxNode info) {
@@ -30,18 +36,29 @@ public class SemanticPass extends VisitorAdaptor {
 		log.info(msg.toString());
 	}
 
+    // SUCCESSFUL PASS
+    public boolean success(){
+        return !errorDetected;
+    }
+
     // PROGRAM
+    @Override
     public void visit(ProgName progName) {
         progName.obj = SymbolTable.insert(Obj.Prog, progName.getProgName(), SymbolTable.noType);
         SymbolTable.openScope();
     }
 
+    @Override
     public void visit(Program program) {
         SymbolTable.chainLocalSymbols(program.getProgName().obj);
         SymbolTable.closeScope();
+        if(!mainExists){
+            report_error("Ne postoji metoda void main()", program);
+        }
     }
 
     // TYPE
+    @Override
     public void visit(Type type){
         Obj typeNode = SymbolTable.find(type.getTypeName());
 		if (typeNode == SymbolTable.noObj) {
@@ -58,11 +75,13 @@ public class SemanticPass extends VisitorAdaptor {
     }
 
     // CONST
+    @Override
     public void visit(NumberConst constant){
         if (currentType == SymbolTable.intType) {
             Obj obj = SymbolTable.currentScope.findSymbol(constant.getConstName());
             if (obj == null) {
                 SymbolTable.insert(Obj.Con, constant.getConstName(), currentType);
+                report_info("Deklarisana konstanta " + constant.getConstName(), constant);
             } else {
                 report_error("Simbol " + constant.getConstName() + " je vec deklarisan", constant);
             }
@@ -71,11 +90,13 @@ public class SemanticPass extends VisitorAdaptor {
         }
     }
 
+    @Override
     public void visit(CharConst constant){
         if (currentType == SymbolTable.charType) {
             Obj obj = SymbolTable.currentScope.findSymbol(constant.getConstName());
             if (obj == null) {
                 SymbolTable.insert(Obj.Con, constant.getConstName(), currentType);
+                report_info("Deklarisana konstanta " + constant.getConstName(), constant);
             } else {
                 report_error("Simbol " + constant.getConstName() + " je vec deklarisan", constant);
             }
@@ -84,16 +105,80 @@ public class SemanticPass extends VisitorAdaptor {
         }
     }
 
+    @Override
     public void visit(BoolConst constant){
         if (currentType == SymbolTable.boolType) {
             Obj obj = SymbolTable.currentScope.findSymbol(constant.getConstName());
             if (obj == null) {
                 SymbolTable.insert(Obj.Con, constant.getConstName(), currentType);
+                report_info("Deklarisana konstanta " + constant.getConstName(), constant);
             } else {
                 report_error("Simbol " + constant.getConstName() + " je vec deklarisan", constant);
             }
         } else {
             report_error("Tip konstante " + constant.getConstName() + " nije ekvivalentan tipu proslednjenog podatka", constant);
         }
+    }
+
+    // VAR
+    @Override
+    public void visit(VarElem var){
+        Obj obj = SymbolTable.currentScope.findSymbol(var.getVarName());
+        if (obj == null) {
+            SymbolTable.insert(Obj.Var, var.getVarName(), currentType);
+            report_info("Deklarisana promenljiva " + var.getVarName(), var);
+        } else {
+            report_error("Simbol " + var.getVarName() + " je vec deklarisan", var);
+        }
+    }
+
+    @Override
+    public void visit(VarArray var){
+        Obj obj = SymbolTable.currentScope.findSymbol(var.getVarName());
+        if (obj == null) {
+            SymbolTable.insert(Obj.Var, var.getVarName(), new Struct(Struct.Array, currentType));
+            report_info("Deklarisana promenljiva " + var.getVarName(), var);
+        } else {
+            report_error("Simbol " + var.getVarName() + " je vec deklarisan", var);
+        }
+    }
+
+    //METHOD
+    public static boolean checkMain(MethodDecl method){
+        return (
+            method.getMethodPars() instanceof NoMethodParameters
+            && method.getMethodName() instanceof VoidMethodName
+            && "main".equals(((VoidMethodName) method.getMethodName()).getMethodName())
+        );
+    }
+
+    @Override
+    public void visit(TypeMethodName methodName) {
+        methodName.obj = currentMethod = SymbolTable.insert(Obj.Meth, methodName.getMethodName(), currentType);
+        report_info("Deklarisana metoda " + methodName.getMethodName(), methodName);
+        SymbolTable.openScope();
+    }
+
+    @Override
+    public void visit(VoidMethodName methodName) {
+        methodName.obj = currentMethod = SymbolTable.insert(Obj.Meth, methodName.getMethodName(), SymbolTable.noType);
+        report_info("Deklarisana metoda " + methodName.getMethodName(), methodName);
+        SymbolTable.openScope();
+    }
+
+    @Override
+    public void visit(MethodDecl method) {
+        if(checkMain(method)){
+            mainExists = true;
+        }
+
+        if (!returnFound && method.getMethodName().obj.getType() != SymbolTable.noType) {
+            report_error("Metoda " + method.getMethodName().obj.getName() + " nema return iskaz", method);
+        }
+
+        SymbolTable.chainLocalSymbols(currentMethod);
+        SymbolTable.closeScope();
+
+        currentMethod = null;
     }
 }
